ca0d4a11e0c43fd8fac63d23fd2df2109564b202
alopezdomingo <analopezdomingo@hotmail.com>
2017-12-13 17:21:00 +0100

    Menú para cenas

---
+++ WebContent/resources/script.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+ function prueba_1(){
+ alert("Hola Mundo!");}
+function prueba_resultados(){
+	var entrantes = ['Tartar de atún','Tartar de Salmón','Cóctel de gambas','Crema de puerros', 'Crema de calabaza'];
+	document.getElementById("prueba").innerHTML = entrantes[Math.floor(Math.random() * entrantes.length)];
+}
+	
+
